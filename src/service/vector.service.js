277616dd57b0ce40d93a7ca7d50ad978efa901ac
Shayan banik <shayan@Shayans-MacBook-Air.local>
2025-09-26 09:08:12 +0530

    Add Pinecone integration and enhance AI response generation with vector memory

---
+++ src/service/vector.service.js
@@ -0,0 +1,29 @@
+const { Pinecone } = require('@pinecone-database/pinecone')
+
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+
+const chatGPTIndex = pc.Index("chatgpt");
+
+async function createMemory({ vectors, metadata, messageId }) {
+  await chatGPTIndex.upsert([
+    {
+      id: messageId,
+      values: vectors,
+      metadata,
+    },
+  ]);
+}
+
+async function queryMemory({ queryVector, limit = 5, metadata }) {
+  const data = await chatGPTIndex.query({
+    vector: queryVector,
+    topK: limit,
+    filter: metadata ? { metadata } : undefined,
+    includeMetadata: true,
+  });
+
+  return data.matches;
+}
+
+
+module.exports = { createMemory, queryMemory };
--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,10 +1,10 @@
 const { Server } = require("socket.io");
 const jwt = require("jsonwebtoken");
-const cookie = require("cookie"); // 👈 helps parse cookies
+const cookie = require("cookie");
 const userModel = require("../models/user.model");
-const  generateResponse  = require("../service/ai.service");
-// const aiService = require("../service/ai.service");
+const { generateResponse, generateVector } = require("../service/ai.service");
 const messageModel = require("../models/message.model");
+const { createMemory, queryMemory } = require("../service/vector.service");
 
 async function initSocketServer(httpServer) {
   const io = new Server(httpServer, {});
@@ -42,33 +42,88 @@ async function initSocketServer(httpServer) {
     //user sends a message to the chat
     socket.on("user-message", async (messagePayload) => {
       // Store the message in the database(Which send by the user)
-      await messageModel.create({
+      const message = await messageModel.create({
         user: socket.user._id,
         content: messagePayload.content,
         chat: messagePayload.chat,
         role: "user",
       });
 
-      const chatHistory = await messageModel.find({
-        chat: messagePayload.chat,
+      //use vector database to get context
+      const vectors = await generateVector(messagePayload.content);
+
+      // Query the vector database to get relevant context(long term memory)
+      const memory = await queryMemory({
+        queryVector: vectors,
+        topK: 3,
+        metadata: {},
       });
 
-      const response = await generateResponse(
-        chatHistory.map((message) => {
-          return {
-            role: message.role,
-            parts: [{ text: message.content }],
-          };
-        })
-      );
-      // const response = await aiService.generateResponse(messagePayload.content);
+      //store the vectors in the vector database(pinecone database)
+      await createMemory({
+        vectors,
+        metadata: {
+          chatId: messagePayload.chat,
+          userId: socket.user._id,
+          text: messagePayload.content,
+        },
+        messageId: message._id,
+      });
+      console.log("Vectors:", vectors);
 
-      await messageModel.create({
+      const chatHistory = (
+        await messageModel
+          .find({
+            chat: messagePayload.chat,
+          })
+          .sort({ createdAt: -1 })
+          .limit(20)
+          .lean()
+      ).reverse();
+
+      const stm = chatHistory.map((message) => {
+        return {
+          role: message.role,
+          parts: [{ text: message.content }],
+        };
+      });
+
+      const ltm = [
+        {
+          role: "user",
+          parts: [
+            {
+              text: `These are the most relevant pieces of information I found in your past conversations:${memory
+                .map((item) => item.metadata.text)
+                .join("\n")}`,
+            },
+          ],
+        },
+      ];
+      
+
+      const response = await generateResponse([...ltm, ...stm]);
+
+      const responseMessage = await messageModel.create({
         user: socket.user._id,
         content: response,
         chat: messagePayload.chat,
         role: "model",
       });
+
+      const responseVectors = await generateVector(response);
+
+      await createMemory({
+        vectors: responseVectors,
+        metadata: {
+          chatId: messagePayload.chat,
+          userId: socket.user._id,
+          text: response,
+        },
+        messageId: responseMessage._id,
+      });
+
+      console.log("Response Vectors:", responseVectors);
 
       socket.emit("ai-response", {
         // Emit AI response to the user
